2b6c68b414fc1f8323b81c65495d138b4207e927
Dan Esrey <danesrey@dans-macbook-air.local>
2016-08-22 14:42:43 -0400

    Initial Commit

---
+++ Tiny Address Book/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Tiny Address Book
+//
+//  Created by Dan Esrey on 2016/22/08.
+//  Copyright © 2016 Dan Esrey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
